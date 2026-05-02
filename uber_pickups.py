b4652482a76b65f0ffbc20c1753877663482f2b6
trevlee31 <trevor.lee@sjsu.edu>
2026-05-02 09:39:57 -0700

    uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Ashraf's App: Uber pickups in NYC")
+st.title("Trevor's App: Uber pickups in NYC")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
